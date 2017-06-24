0f771522ec1a5fcb4ac89f7ea989d3ee02a79789
reubrester <rrester@gmail.com>
2017-06-23 21:59:59 -0500

    Added a link to my linkedin page and to my github repository 6/23/2017 21:59

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,3 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/IndianMotocycle.jpg') {
-      myImage.setAttribute ('src','images/IndianMotocycle7.jpg');
-    } else {
-      myImage.setAttribute ('src','images/IndianMotocycle.jpg');
-    }
-  }
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name' , myName);
-  myHeading.textContent = 'This is a cool website, ' + myName;
-}
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'This is a cool website, ' + storedName;
-}
-myButton.onclick = function() {
-  setUserName();
-}
+<script
+src="https://platform.linkedin.com/badges/js/profile.js" async defer>
+</script>
